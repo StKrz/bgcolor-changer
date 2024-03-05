4341f76ae39b03992095ec11cf038621c402ac61
StKrz <stavroskyriazopoulos@gmail.com>
2024-03-05 10:37:11 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+let bgColor = undefined
+window.addEventListener('DOMContentLoaded', function() {
+
+    this.document.querySelector('#btn').addEventListener('click', function() {
+        onClickMeClicked()
+    })
+    
+})
+
+function onClickMeClicked() {
+    updateBg()
+}
+
+// Model
+
+
+/**
+ * Gets a color and updates the UI Element.
+ */
+function updateBg() {
+    bgColor = getBgColor()
+    showBgColor()
+}
+
+
+/**
+ *  Selects a random color from a standard list.
+ * @returns the color.
+ */
+//ειναι αυτη που θα φερει το τυχαιο χρωμα απο εναν πινακα
+function getBgColor() {
+    const colors = ['red', 'white', 'black', 'green', 'blue', 'yellow']
+    return colors[Math.floor(Math.random() * colors.length)] // θα φερει απο το 0 εως length-1
+}
+
+
+/**
+ * Fills the values of the corresponding UI elements.
+ */
+function showBgColor() {
+    document.querySelector('#clr-hex').innerHTML = bgColor
+    document.body.style.backgroundColor = bgColor
+}
